674996cb6f9bbbfe697725d76ef847519d38e357
Able <munirammar0@gmail.com>
2023-08-09 22:47:50 +0500

    Writing models and migrating them

--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    pass
+
+
+
+class LeadModel(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    agent = models.ForeignKey("Agent",on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.first_name
+    
+
+
+
+class Agent(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
