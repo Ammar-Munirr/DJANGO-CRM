68698a2a49171746e3ef619f8b4c8bcd4c2382b1
Able <munirammar0@gmail.com>
2023-08-10 21:52:26 +0500

    Adding Lead Updating function

--- leads/urls.py
+++ leads/urls.py
@@ -5,5 +5,6 @@ app_name = 'leads'
 urlpatterns = [
     path('list/',views.LeadList.as_view(),name='lead-list'),
     path('<int:pk>/detail/',views.LeadDetailView.as_view(),name='lead-detail'),
-    path('create-lead/',views.LeadCreateView.as_view(),name='lead-create')
+    path('create-lead/',views.LeadCreateView.as_view(),name='lead-create'),
+    path('<int:pk>/update-lead/',views.LeadUpdateView.as_view(),name='lead-update')
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,6 @@
 from django.forms.models import BaseModelForm
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,reverse
 from django.views import generic
 from .models import Agent
 from .models import LeadModel
@@ -27,3 +27,11 @@ class LeadCreateView(generic.CreateView):
     template_name = 'leads/lead-create.html'
     form_class = LeadModelForm
     success_url = '/leads/list'
+
+
+class LeadUpdateView(generic.UpdateView):
+    template_name = 'leads/lead-update.html'
+    queryset = LeadModel.objects.all()
+    form_class = LeadModelForm
+    def get_success_url(self):
+        return reverse('leads:lead-list')
