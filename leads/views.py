31928035936a6b744c31781adead516754549f7d
Able <munirammar0@gmail.com>
2023-08-11 21:15:30 +0500

    Adding Base template and setting up the template root directory

--- leads/views.py
+++ leads/views.py
@@ -9,24 +9,20 @@ from .forms import LeadModelForm
 class Home(generic.TemplateView):
     template_name = 'leads/home.html'
 
-
 class LeadList(generic.ListView):
     template_name = 'leads/lead_list.html'
     queryset = LeadModel.objects.all()
     context_object_name = 'leads'
-
 
 class LeadDetailView(generic.DetailView):
     template_name = 'leads/lead-detail.html'
     queryset = LeadModel.objects.all()
     context_object_name = 'lead'
 
-
 class LeadCreateView(generic.CreateView):
     template_name = 'leads/lead-create.html'
     form_class = LeadModelForm
     success_url = '/leads/list'
-
 
 class LeadUpdateView(generic.UpdateView):
     template_name = 'leads/lead-update.html'
